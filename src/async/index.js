682df13eebf3dbb06716841fb29dab77847531f9
Diego Avellaneda <daacdiego2015@gmail.com>
2024-06-09 11:53:00 -0500

    Agregamos modificaciones en el package.json porque no funcionaba y agregamos llamdo de función asincrona en async/index.js

--- src/async/index.js
+++ src/async/index.js
@@ -1,14 +1,22 @@
+
+//Creación de la promesa
+
 const fnasync= () => {
     return new Promise((resolve, reject) => {
         (true)
             ? setTimeout(()=>resolve("Async!"), 2000)
             :reject(new Error("Error!"))
-    })
+    });
 }
 
-
+//Creación de la función asincrona 
 const anotherFn = async () =>{
     const something = await fnasync()
     console.log(something)
     console.log("Hello")
 }
+
+
+console.log("Before");
+anotherFn(); //Llamado de la función
+console.log("After")
